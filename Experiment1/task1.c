83a927a20ff77d5fd3886830f6d88808bb523151
Tanishka200605 <tanishka20@eng.rizvi.edu.in>
2025-01-10 14:30:33 +0530

    Create task1.c

---
+++ Experiment1/task1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+printf("Full Name\t: Sanjay Singh\n");
+printf("Age\t\t: 26\n");
+printf("Class\t\t: FE\n");
+printf("Division\t: D\n");
+printf("UIN\t\t: 241P005\n");
+return 0;
+}
